58177fd9add30e6f75cdc7f5885e74439cebf863
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-05-18 10:39:42 -0500

    solution 5/18/22

---
+++ 8-kyu/sumMixedArray.js
@@ -0,0 +1,10 @@
+/* Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+function sumMix(x){
+    let numArray = x.map(e => Number(e))
+    let sum = numArray.reduce((acc, i) => acc + i, 0)
+    return sum
+  }
+

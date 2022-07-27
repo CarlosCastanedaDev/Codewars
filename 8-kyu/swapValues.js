8b3be08f8727b986ebc751a344c65dd21bc800c7
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-07-27 11:34:10 -0500

    solution 7/27/22

---
+++ 8-kyu/swapValues.js
@@ -0,0 +1,11 @@
+/* I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here? */
+
+function swapValues(arr) {
+    var args = arr
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+  return args
+}

f940c08d0497d18ccf4ad61e5a81e1eb9e96ba52
CarlosCastanedaDev <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-08-28 13:05:42 -0500

    solution 8/28/22

--- 8-kyu/nameOnBillboard.js
+++ 8-kyu/nameOnBillboard.js
@@ -6,4 +6,4 @@ If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 
 
 function billboard(name, price = 30){
     return Math.imul(name.split('').length, price)
-  } 
+  }

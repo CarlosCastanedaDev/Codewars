47b99d94bdf3a7804560a49b0446d89d2900ec52
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-10-15 10:12:41 -0500

    solution 10/15/22

--- 7-kyu/divideAndConquer.js
+++ 7-kyu/divideAndConquer.js
@@ -2,15 +2,15 @@
 
 Return as a number. */
 
-function divCon(x){
-    let nums = 0
-    let str = 0
-    for(let i = 0; i < x.length; i++){
-      if(Number.isInteger(x[i])){
-        nums += x[i]
-      }else {
-        str += Number(x[i])
-      }
+function divCon(x) {
+  let nums = 0;
+  let str = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (Number.isInteger(x[i])) {
+      nums += x[i];
+    } else {
+      str += Number(x[i]);
     }
-    return nums - str
   }
+  return nums - str;
+}

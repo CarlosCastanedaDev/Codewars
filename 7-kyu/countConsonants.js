b116ad250e17ff5e13a206822f24a74721b9a4b3
CarlosCastanedaDev <carlos@nemin.dev>
2023-01-06 09:18:08 -0600

    solution 1/6/23

--- 7-kyu/countConsonants.js
+++ 7-kyu/countConsonants.js
@@ -1,6 +1,6 @@
 /* Write a function consonantCount, consonant_count or ConsonantCount that takes a string of English-language text and returns the number of consonants in the string.
 
-Consonants are all letters used to write English excluding the vowels a, e, i, o, u. */
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.  */
 
 function consonantCount(str) {
   let count = 0;

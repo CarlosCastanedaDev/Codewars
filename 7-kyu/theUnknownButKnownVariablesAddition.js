a0c09198bb6730e506a83b7bb59b4e9294bb7b49
CarlosCastanedaDev <carlos@nemin.dev>
2023-01-27 09:31:24 -0600

    solution 1/27/23

---
+++ 7-kyu/theUnknownButKnownVariablesAddition.js
@@ -0,0 +1,24 @@
+/* I know the answers, it is your task to find out.
+
+Once you crack the code for one or two of the pairs, you will have the answer for the rest.
+
+It is like when you were in school doing math and you saw "11 = c+h" and you needed to find out what c and h were.
+
+However you don't have an 11. You have an unknown there as well. Example:
+
+X = a+b.
+
+You don't know what X is, and you don't know what b is or a, but it is a puzzle and you will find out.
+
+As part of this puzzle, there is three hints or clues on solving this. I won't tell you what the other two are, but one of them is: Don't overthink it. It is a simple solution 
+
+Given the input as a string - Return the sum of the two variables as int. */
+
+function theVar(theVariables) {
+  let sum = 0;
+  for (let i = 0; i < theVariables.length; i++) {
+    if (theVariables[i] != '+') sum += theVariables[i].charCodeAt() - 96;
+  }
+
+  return sum;
+}

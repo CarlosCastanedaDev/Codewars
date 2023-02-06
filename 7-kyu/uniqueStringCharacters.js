c143dfa8e2d63388fe620ae620625cb1715370a5
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-02-06 11:09:15 -0600

    solution 2/6/23

---
+++ 7-kyu/uniqueStringCharacters.js
@@ -0,0 +1,27 @@
+/* In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+
+For example:
+
+solve("xyab","xzca") = "ybzc" 
+--The first string has 'yb' which is not in the second string. 
+--The second string has 'zc' which is not in the first string. 
+Notice also that you return the characters from the first string concatenated with those from the second string.
+
+More examples in the tests cases.
+
+Good luck! */
+
+function solve(a, b) {
+  let newStr = '';
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      newStr += a[i];
+    }
+  }
+  for (let j = 0; j < b.length; j++) {
+    if (!a.includes(b[j])) {
+      newStr += b[j];
+    }
+  }
+  return newStr;
+}

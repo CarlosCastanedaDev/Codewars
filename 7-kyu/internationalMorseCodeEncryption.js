9fc6612436def2cf67e923e46be7117e9ced1a40
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-03-13 12:08:34 -0500

    solution 3/13/23

---
+++ 7-kyu/internationalMorseCodeEncryption.js
@@ -0,0 +1,21 @@
+/* Write a function that will encrypt a given sentence into International Morse Code, both the input and out puts will be strings.
+
+Characters should be separated by a single space. Words should be separated by a triple space.
+
+For example, "HELLO WORLD" should return -> ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
+
+To find out more about Morse Code follow this link: https://en.wikipedia.org/wiki/Morse_code
+
+A preloaded object/dictionary/hash called CHAR_TO_MORSE will be provided to help convert characters to Morse Code. */
+
+function encryption(message) {
+  let morse = '';
+  for (let i = 0; i < message.length; i++) {
+    if (CHAR_TO_MORSE[message[i]]) {
+      morse += CHAR_TO_MORSE[message[i]] + ' ';
+    } else {
+      morse += '  ';
+    }
+  }
+  return morse.trim();
+}
